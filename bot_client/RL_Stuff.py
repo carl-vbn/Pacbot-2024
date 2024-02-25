ead995eea292a9f60c81f8a8ce1879b87c3ada5e
saloni-jain-code <sjain102015@gmail.com>
2024-02-25 16:49:55 -0500

    outlined step function and added some helper methods

--- bot_client/RL_Stuff.py
+++ bot_client/RL_Stuff.py
@@ -6,35 +6,46 @@ import numpy as np
 
 class RLLearn_SARAS():
     def __init__(self, addr, port):
-        self.subscriptions = [MsgType.LIGHT_STATE]
-        super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
-        self.state = None
-        self.grid = copy.deepcopy(grid)
+        # self.subscriptions = [MsgType.LIGHT_STATE]
+        # super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
+        # self.state = None
+        # self.grid = copy.deepcopy(grid)
 
-        self.policy = np.zeros((grid.shape, 4))
-        self.lr = 0.01
-        self.min_lr = 0.001
-        self.lr_decay = 0.99
-        self.gamma = 1
-        self.eps = 0.1
-        self.eps_decay = 0.999
-        self.min_eps = 0.001
-        self.Q = np.zeros() # the policy
-        self.gamma = 0
-        self.alpha = 0
-        return
+        # self.policy = np.zeros((grid.shape, 4))
+        # self.lr = 0.01
+        # self.min_lr = 0.001
+        # self.lr_decay = 0.99
+        # self.gamma = 1
+        # self.eps = 0.1
+        # self.eps_decay = 0.999
+        # self.min_eps = 0.001
+        # self.Q = np.zeros() # the policy
+        # self.gamma = 0
+        # self.alpha = 0
+        pass
 
+
+    def update_game_state(self):
+        pass
+
+    def get_reward(self):
+        done = self.is_done() #return true if eaten all coins
+        dead = self.is_dead()
+        pass
+
+    def get_action_greedy(self, state):
+        action = 0
+        return action
+    
+    def update_Q(self):
+        pass
+    
     def is_done(self):
-        done = True
-        for i in self.grid.shape[0]:
-            for j in self.grid.shape[1]:
-                if self.grid[i][j] in [o, O]:
-                    done = False
-        return done
+        pass
     
     def is_dead(self, state):
-        return self.grid[state[0]][state[1]]
-    
+        pass
+
     #Function to learn the Q-value 
     # source: https://www.geeksforgeeks.org/sarsa-reinforcement-learning/
     def update(self, state, state2, reward, action, action2):
@@ -42,53 +53,31 @@ class RLLearn_SARAS():
         target = reward + self.gamma * Q[state2, action2]
         self.Q[state, action] = self.Q[state, action] + self.alpha * (target - predict)
     
-    def stepping(self, past_state, past_action):
-        #Updating the new state, the reward for the step, whether pacman is done or not
+    def step(self, current_state, current_action):
+        # Updating the new state, the reward for the step, whether pacman is done or not
+        # perform the action a to get to the next state (s')
+        new_state = self.update_game_state(current_state, current_action) # s'
+
+        # get reward (r) for moving to the next state (s')
+        reward = self.get_reward(current_state, current_action, new_state)
         
-        ## OLD CODE
+        # get action_greedy a' based on the next state s'
+        action_greedy = self.get_action_greedy(new_state) # a'
         
-        state = past_state
-        eat_pellet = False
-        if self.state and self.state.mode == LightState.RUNNING:
-            state = (self.state.pacman.x, self.state.pacman.y)
+        # find Q(s', a')
+        self.update_Q(new_state, action_greedy)
 
-        # update game state, we have eaten the little thing good for us
-        if self.grid[state[0]][state[1]] in [o, O]:
-            self.grid[state[0]][state[1]] = e
-            eat_pellet = True
-        
-        
-        ## GET THE NEXT ACTION BASED ON EPSILON
-        action = self.get_action_epsilon(state, False)
-        print(action) #action is equivilant of path[1], it should be of the form,
-        command = self.action_to_command(action)
-        print(command)
-
-        done = self.is_done() #return true if eaten all coins
-        dead = self.is_dead()
-        reward = self.get_reward(action, state, done, past_action, eat_pellet, dead, past_state)
-       
-        if action != None:
-            # Figure out position we need to move
-            new_msg = PacmanCommand()
-            new_msg.dir = self._get_direction(state, action)
-            self.write(new_msg.SerializeToString(), MsgType.PACMAN_COMMAND)
-            return
-
-        new_msg = PacmanCommand()
-        new_msg.dir = PacmanCommand.STOP
-        self.write(new_msg.SerializeToString(), MsgType.PACMAN_COMMAND)
-
-
-
-        return self.get_next_state(action), reward, done, dead
+        return new_state, action_greedy, reward
     
-    def get_action_random():
-        return
-        
-    def get_action_greedy():
-        return
-        
+    def train(self, initial_state, initial_action):
+        current_state = initial_state
+        current_action = initial_action
+        # call step unless is done or is dead
+        while not (self.is_done() or self.is_dead()):
+            new_state, action_greedy, reward = self.step(current_state, current_action)
+            current_state = new_state
+            current_action = action_greedy
+    
     def get_action_epsilon(self, state):
         # will call get action random or get action greedy depending on epsilon
         action = 0
@@ -97,9 +86,6 @@ class RLLearn_SARAS():
         else:
             action = self.get_action_greedy()
         return action
-
-
-        return
     
     def calculate_reward(state1, state2, action):
         return 0
@@ -107,10 +93,6 @@ class RLLearn_SARAS():
     def action_to_command():
         return
     
-    def train():
-        return
-
-
     def evaluate():
         return
 
